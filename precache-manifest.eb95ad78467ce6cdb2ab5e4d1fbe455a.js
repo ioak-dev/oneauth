9448f28699f13a33e05e858f5da98b66ecfafeb9
Arun Kumar Selvaraj <arunkumar.selvaraj@westernacher.com>
2020-05-11 13:58:12 +0530

    Updates

---
+++ precache-manifest.eb95ad78467ce6cdb2ab5e4d1fbe455a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e501969ec8ee0eae2844c4f50df6ea33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5115af87d022f20554ca",
+    "url": "/static/css/main.4cab6d77.chunk.css"
+  },
+  {
+    "revision": "9330e09adc29e81fa1bc",
+    "url": "/static/js/2.217e6da8.chunk.js"
+  },
+  {
+    "revision": "e89ca22e023f5a385d87cd8aba699af6",
+    "url": "/static/js/2.217e6da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5115af87d022f20554ca",
+    "url": "/static/js/main.98eb1dc0.chunk.js"
+  },
+  {
+    "revision": "05f2cadbeef4945f426c",
+    "url": "/static/js/runtime-main.13ed9aa6.js"
+  },
+  {
+    "revision": "0ba1fd526d8077d74d8df364532da658",
+    "url": "/static/media/ioak_black.0ba1fd52.svg"
+  },
+  {
+    "revision": "39c878d7124286fc417ec03b7ea125c9",
+    "url": "/static/media/ioak_white.39c878d7.svg"
+  },
+  {
+    "revision": "4723b18dd533a20cb08c4f35529a4197",
+    "url": "/static/media/login_bg.4723b18d.jpg"
+  },
+  {
+    "revision": "24d2a5118d14704bd1b632916fdcbbc7",
+    "url": "/static/media/login_bg_oa.24d2a511.jpg"
+  },
+  {
+    "revision": "a43e1f6ba23f9bfc4a5d9a9488c56f07",
+    "url": "/static/media/manageapp.a43e1f6b.jpg"
+  },
+  {
+    "revision": "7aed2e3864522998087fa67b1c5435ce",
+    "url": "/static/media/mirror_white_small.7aed2e38.svg"
+  }
+]);
